103be257127045adb7ac0fa24ca47b0dabfa2293
[Niggle Nandu] <nigglenandu@gmail.com>
2025-02-13 18:28:46 +0545

    string changed

--- String.py
+++ String.py
@@ -1,10 +1,19 @@
-name = "Niggle"
-print(name[2])
-print(len(name))
-print("l" in name)
-print("l" not in name)
-print(name[2:4])
-print(name.upper())
+#String
+#single quotation marks, or double quotation marks.
+
+name = "Niggle, Nandu "
+a= 5
+print(name[2]) #g
+print(len(name)) #14
+print("l" in name)  #True
+print("l" not in name) #Flase
+print(name[2:4]) #gg
+print(name.upper()) #NIGGLE, NANDU
+print(name.strip()) #remove white space frm beginning or end
+print(name.replace("Niggle, Nandu",  "Nandu"))
+print(name.split(",")) #['Niggle', ' Nandu ']
+print("Hello " + name) #concat
+
 
 #loop 
 for x in name:
